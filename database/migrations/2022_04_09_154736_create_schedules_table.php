06fc45c72e9f61d7bf47a197db91dd0be2a819d5
alansyue <b123105@gmail.com>
2022-04-09 23:53:06 +0800

    migration: create a schedule table

---
+++ database/migrations/2022_04_09_154736_create_schedules_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Trip;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Trip::class);
+            $table->enum('type', ['attraction', 'break']);
+            $table->string('name');
+            $table->string('address');
+            $table->string('start_time');
+            $table->float('duration');
+            $table->float('traffic_time');
+            $table->float('lat');
+            $table->float('long');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
